4566b2a193c9386ec4600d88f97cd68c67d17cec
laichunling <laichunling@joyy.com>
2023-05-03 21:22:13 +0800

    update：按ide建议的优化下代码

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"awesomeProject/routes"
 )
 
-var db = make(map[string]string)
-
 func main() {
 	r := routes.SetupRouter()
 	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	err := r.Run(":8080")
+	if err != nil {
+		return
+	}
 }
